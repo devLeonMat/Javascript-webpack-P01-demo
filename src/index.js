b3553b8da89f57ad50239fd7b2c91e9a731b64cc
leon matias <leonmatias1991@gmail.com>
2020-12-23 17:38:27 -0500

    subiendo cambios iniciales

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {saludar} from './js/components'
+
+const name = 'leon matias';
+
+saludar(name);
---
+++ src/js/components.js
@@ -0,0 +1,8 @@
+const saludar = (name) => {
+    console.log('creando etiqueta');
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola, ${name}`;
+
+    document.body.append(h1);
+};
+
